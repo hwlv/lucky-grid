26a002960455bca996fccad85e17e337c08043f2
stone <likeawang@qq.com>
2024-07-17 23:54:56 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// index.ts
+export function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// index.ts
+export function addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+  }
